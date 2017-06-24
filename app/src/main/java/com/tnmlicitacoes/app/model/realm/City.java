1834f9c87140293d730c4298e323057311781b40
Diego Bezerra <dsouzaduarte@gmail.com>
2017-06-24 10:19:37 -0300

    Add city

---
+++ app/src/main/java/com/tnmlicitacoes/app/model/realm/City.java
@@ -0,0 +1,52 @@
+package com.tnmlicitacoes.app.model.realm;
+
+import com.tnmlicitacoes.app.NoticesQuery;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class City extends RealmObject {
+
+    @PrimaryKey
+    private String id;
+    private String name;
+    private String state;
+
+    public City() {
+
+    }
+
+    public City(String id, String name, String state) {
+        this.id = id;
+        this.name = name;
+        this.state = state;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public static City copyToRealmFromGraphQL(NoticesQuery.City city) {
+        return new City(city.id(), city.name(), city.state().name());
+    }
+}
